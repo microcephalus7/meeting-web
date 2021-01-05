28c90b408f6bb1b91c5b7ce5c6ff2b9d4363a1ad
허석호 <microcephalus7@gmail.com>
2021-01-05 14:27:36 +0900

    댓글 model

--- backend/community/models.py
+++ backend/community/models.py
@@ -3,11 +3,19 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
-    body = models.TextField()
+    body = models.TextField(max_length=200)
     pubDate = models.DateTimeField('date published')
 
     def __str__(self):
         return self.title
 
 
-# Create your models here.
+class Comment (models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    body = models.TextField(max_length=100)
+    pubDate = models.DateTimeField('date published')
+
+    def __str__(self):
+        if len(self.body) > 20:
+            return self.body[0:20] + '...'
+        return self.body
