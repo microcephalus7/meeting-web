bf2ac70cc3296f04c2aab4b73d2e4b1d9e69dd27
허석호 <microcephalus7@gmail.com>
2021-01-13 15:41:16 +0900

    토큰 확인 및 유저 정보 데코레이터 함수 생성

---
+++ backend/core/utils.py
@@ -0,0 +1,21 @@
+import jwt
+import json
+from django.http import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
+from backend.settings import SECRET_KEY
+from account.models import Account
+
+
+def tokenCheckDecorator(func):
+    def wrapper(request, *args, **kwargs):
+        try:
+            token = request.COOKIES["token"]
+            token = jwt.decode(token, SECRET_KEY, algorithms="HS256")
+            account = Account.objects.get(email=token["email"])
+            request.account = account
+        except jwt.exeptions.DecodeError:
+            return JsonResponse('유효하지 않은 토큰', status=400)
+        except Account.DoesNotExist:
+            return JsonResponse('해당 유저가 없습니다', status=400)
+        return func(request, *args, **kwargs)
+    return wrapper
