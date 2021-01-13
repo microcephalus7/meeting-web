27f235c60aa669413bd6096527fe57c28a0c96de
허석호 <microcephalus7@gmail.com>
2021-01-13 13:09:50 +0900

    회원가입, 로그인 토큰 부착 및 비밀번호 암호화

--- backend/account/views.py
+++ backend/account/views.py
@@ -6,6 +6,9 @@ import json
 from django.utils import timezone
 from django.core import serializers
 from django.views.decorators.csrf import csrf_exempt
+import bcrypt
+import jwt
+from backend.settings import SECRET_KEY
 
 # Create your views here.
 
@@ -19,26 +22,38 @@ def index(request):
         if accountCheck.exists():
             return JsonResponse('이미 가입 이력이 존재합니다', safe=False)
         else:
+            password = data["password"].encode('utf-8')
+            passwordCrypt = bcrypt.hashpw(password, bcrypt.gensalt())
+            passwordCrypt = passwordCrypt.decode('utf-8')
             newAccount = Account(
-                email=data["email"], password=data["password"])
+                email=data["email"], password=passwordCrypt)
             newAccount.save()
-            result = model_to_dict(newAccount, fields=('email'))
-            return JsonResponse(result, safe=False)
+            token = jwt.encode({'email': newAccount.email, 'exp': timezone.now()+timezone.timedelta(days=7)},
+                               SECRET_KEY, algorithm="HS256")
+            result = JsonResponse(model_to_dict(
+                newAccount, fields=('email')), safe=False)
+            result.set_cookie('token', token)
+            return result
 
     # 로그인
     if request.method == "GET":
         if accountCheck.exists():
             account = Account.objects.get(email=data["email"])
-            if account.password == data["password"]:
-                accountDict = model_to_dict(account, fields=("email"))
-                return JsonResponse(accountDict, safe=False)
+            if bcrypt.checkpw(data["password"].encode('utf-8'), account.password.encode('utf-8')):
+                token = jwt.encode({'email': account.email, 'exp': timezone.now(
+                )+timezone.timedelta(days=7)}, SECRET_KEY, algorithm="HS256")
+                result = JsonResponse(model_to_dict(
+                    account, fields=("email")), safe=False)
+                result.set_cookie('token', token)
+                return result
             else:
                 return JsonResponse('비밀번호가 일치하지 않습니다', safe=False)
         else:
             return JsonResponse('계정이 존재하지 않습니다', safe=False)
     # 회원 수정
     if request.method == "PATCH":
-        return HttpResponse(request)
+        return JsonResponse(request)
+    return result
     # 회원탈퇴
     if request.method == "DELETE":
         return HttpResponse(request)
