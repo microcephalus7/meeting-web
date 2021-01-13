3b187a4cf10ad94233ea96759ba00678440c80bb
허석호 <microcephalus7@gmail.com>
2021-01-13 13:57:34 +0900

    토큰 검증 api 이슈 픽스 중

--- backend/account/views.py
+++ backend/account/views.py
@@ -5,7 +5,9 @@ from .models import Account
 import json
 from django.utils import timezone
 from django.core import serializers
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from django.views import View
 import bcrypt
 import jwt
 from backend.settings import SECRET_KEY
@@ -21,19 +23,19 @@ def index(request):
     if request.method == "POST":
         if accountCheck.exists():
             return JsonResponse('이미 가입 이력이 존재합니다', safe=False)
-        else:
-            password = data["password"].encode('utf-8')
-            passwordCrypt = bcrypt.hashpw(password, bcrypt.gensalt())
-            passwordCrypt = passwordCrypt.decode('utf-8')
-            newAccount = Account(
-                email=data["email"], password=passwordCrypt)
-            newAccount.save()
-            token = jwt.encode({'email': newAccount.email, 'exp': timezone.now()+timezone.timedelta(days=7)},
-                               SECRET_KEY, algorithm="HS256")
-            result = JsonResponse(model_to_dict(
-                newAccount, fields=('email')), safe=False)
-            result.set_cookie('token', token)
-            return result
+
+        password = data["password"].encode('utf-8')
+        passwordCrypt = bcrypt.hashpw(password, bcrypt.gensalt())
+        passwordCrypt = passwordCrypt.decode('utf-8')
+        newAccount = Account(
+            email=data["email"], password=passwordCrypt)
+        newAccount.save()
+        token = jwt.encode({'email': newAccount.email, 'exp': timezone.now()+timezone.timedelta(days=7)},
+                           SECRET_KEY, algorithm="HS256")
+        result = JsonResponse(model_to_dict(
+            newAccount, fields=('email')), safe=False)
+        result.set_cookie('token', token)
+        return result
 
     # 로그인
     if request.method == "GET":
@@ -46,14 +48,24 @@ def index(request):
                     account, fields=("email")), safe=False)
                 result.set_cookie('token', token)
                 return result
-            else:
-                return JsonResponse('비밀번호가 일치하지 않습니다', safe=False)
-        else:
-            return JsonResponse('계정이 존재하지 않습니다', safe=False)
+
+            return JsonResponse('비밀번호가 일치하지 않습니다', safe=False)
+
+        return JsonResponse('계정이 존재하지 않습니다', safe=False)
     # 회원 수정
     if request.method == "PATCH":
         return JsonResponse(request)
-    return result
+
     # 회원탈퇴
     if request.method == "DELETE":
         return HttpResponse(request)
+
+
+@csrf_exempt
+def tokenCheck(request):
+    token = request.COOKIES["token"]
+    userTokenInfo = jwt.decode(token, SECRET_KEY, algorithm="HS256")
+    if Account.objects.filter(email=userTokenInfo["email"]).exists():
+        result = JsonResponse("검증된 사용자")
+        return result
+    return JsonResponse("미검증 사용자")
