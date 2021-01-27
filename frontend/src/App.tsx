b1c0084470ce80513a777a6fa905d7f49f19028b
microcephalus7 <microcephalus7@gmail.com>
2021-01-27 18:59:11 +0900

    styled-components type 추가 및 테스트

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import styled from "styled-components";
+
+const SideNav = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: black;
+`;
 
 const App: React.FC = () => {
   return (
-    <div>
-      <div>
-        <div></div>
-      </div>
-    </div>
+    <>
+      <SideNav></SideNav>
+    </>
   );
 };
 
